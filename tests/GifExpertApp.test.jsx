154433b5b05bf13eab8367a5d1be09709a5f3c26
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2023-11-09 06:53:01 +0100

    seccion 8 terminada

--- tests/GifExpertApp.test.jsx
+++ tests/GifExpertApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, renderHook, screen } from '@testing-library/react';
 import { GifExpertApp } from '../src/GifExpertApp';
 
 describe('pruebas en GifExpertApp', () => {
@@ -10,9 +10,32 @@ describe('pruebas en GifExpertApp', () => {
 
     });
 
-    test('debe de hacer match con el snapshot', () => {
+    test('debe de no retornar nada si la categoria ya existe', () => {
+
+        const onAddCategory = 'Naruto';
 
         render(<GifExpertApp />)
+
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+
+        fireEvent.input(input, { target: { value: onAddCategory } });
+        fireEvent.submit(form);
+
+        fireEvent.input(input, { target: { value: onAddCategory } });
+        fireEvent.submit(form);
+
+        const h3Elements = screen.getAllByText('Naruto');
+
+        expect(h3Elements.length).not.toBeGreaterThan(1);
+
+
+
+
+
+
+
+
 
 
     });
